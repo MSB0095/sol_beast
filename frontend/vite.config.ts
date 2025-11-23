5623a817520127a1a8d3606f8868e8e78ad4bc07
MSB0095 <msb0095@proton.me>
2025-11-24 00:03:14 +0100

    Add vite-plugin-node-polyfills and update Vite configuration

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
+import wasm from "vite-plugin-wasm";
+import topLevelAwait from "vite-plugin-top-level-await";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    nodePolyfills(),
+    react(),
+    wasm(),
+    topLevelAwait()
+  ],
+  define: {
+    'global.Buffer': 'Buffer'
+  },
   base: process.env.NODE_ENV === 'production' ? '/sol_beast/' : '/',
   build: {
     outDir: 'dist',
